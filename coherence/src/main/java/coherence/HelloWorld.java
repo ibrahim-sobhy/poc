755c4818f93ef1ff1a3609d832332f9f556752b4
Ibrahim <isobhy@etisalat.ae>
2017-02-01 21:44:44 +0400

    Initial coherence poc setup
    To start setup oracle coherence poc hello world example and
    configuration files. Usually we need to write client or server code and
    configurations.

---
+++ coherence/src/main/java/coherence/HelloWorld.java
@@ -0,0 +1,20 @@
+package coherence;
+
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		String key = "k1";
+		String value = "Hello World!";
+		
+		CacheFactory.ensureCluster();
+		NamedCache cache = CacheFactory.getCache("hello-example");
+		
+		cache.put(key, value);
+		
+		System.out.println((String)cache.get(key));
+		
+		CacheFactory.shutdown();
+	}
+}
